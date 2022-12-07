5b8e069fec630e16036aeb7101def1230fc8ac1b
Rock Shen <rock_shen@asus.com>
2022-12-07 18:24:51 +0800

    bluetooth: fix WMX6219 not use rtk_btusb driver
    Change-Id: If33381eac85ac9fe1c455ef5ba3235d7e9a14daa
    Signed-off-by: Rock Shen <rock_shen@asus.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -396,8 +396,7 @@ static const struct usb_device_id blacklist_table[] = {
 	/* Realtek 8822CE Bluetooth devices */
 	{ USB_DEVICE(0x0bda, 0xb00c), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
-	{ USB_DEVICE(0x0bda, 0xc822), .driver_info = BTUSB_IGNORE |
-						     BTUSB_WIDEBAND_SPEECH },
+	{ USB_DEVICE(0x0bda, 0xc822), .driver_info = BTUSB_IGNORE },
 
 	/* Realtek Bluetooth devices */
 	/* { USB_VENDOR_AND_INTERFACE_INFO(0x0bda, 0xe0, 0x01, 0x01),
